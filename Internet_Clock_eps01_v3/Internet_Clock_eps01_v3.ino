3043ef97a85af207928a094b8b226f511c6d3844
Your Name <you@example.com>
2021-09-08 22:14:13 -0300

    atulaizao de codigo

--- Internet_Clock_eps01_v3/Internet_Clock_eps01_v3.ino
+++ Internet_Clock_eps01_v3/Internet_Clock_eps01_v3.ino
@@ -1,10 +1,35 @@
-//CODIGO ESP 8266
+  #include <NTPClient.h>
+  #include <ESP8266WiFi.h>
+  #include <WiFiUdp.h>
+
+  const char *ssid     = "VIVOFIBRA-9501";
+  const char *password = "rgw7ucm3GT";
+
+  const long utcOffsetInSeconds =  -10800;
+  char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"};
+  
+  WiFiUDP ntpUDP;
+  NTPClient timeClient(ntpUDP, "south-america.pool.ntp.org", utcOffsetInSeconds,60000);
+
+  byte  Minuto,
+        Hora;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+    Serial.print(".");
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  timeClient.update();
+  Hora = (timeClient.getHours());
+  Minuto = (timeClient.getMinutes());
+  delay(1000);
 
 }
